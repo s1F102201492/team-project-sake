44d463115d0c70d6bdda4088f9ab7cd3d2a5c95c
s1f10230084 <s1f102300841@iniad.org>
2025-11-12 15:12:34 +0900

    スタンプは1回きり

--- backend/stamprally/models.py
+++ backend/stamprally/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Checkpoint(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    img = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class Stamp(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkpoint = models.ForeignKey(Checkpoint, on_delete=models.CASCADE)
+    obtained_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "checkpoint")
