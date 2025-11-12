aa0c40a8394c97b838244e5fb4bbe5f3f2b14eaf
s1f10230084 <s1f102300841@iniad.org>
2025-11-12 15:58:59 +0900

    スタンプ帳の仮実装

--- backend/stamprally/models.py
+++ backend/stamprally/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Checkpoint(models.Model):
+    name = models.CharField(max_length=100)
+    lat = models.FloatField()
+    lng = models.FloatField()
+    img = models.CharField(max_length=200)  # スタンプ画像のパスやURL
+
+    def __str__(self):
+        return self.name
+
+class Stamp(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checkpoint = models.ForeignKey(Checkpoint, on_delete=models.CASCADE)
+    obtained_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "checkpoint")
--- backend/stamprally/views.py
+++ backend/stamprally/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Checkpoint, Stamp
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    return render(request, 'stamprally/index.html')
+    checkpoints = Checkpoint.objects.all()
+    stamps = Stamp.objects.filter(user=request.user)
+    obtained_ids = [s.checkpoint.id for s in stamps]
+    
+    context = {
+        "checkpoints": checkpoints,
+        "obtained_ids": obtained_ids,
+    }
+    return render(request, 'stamprally/index.html', context)
